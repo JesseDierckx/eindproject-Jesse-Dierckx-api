767246952624663b841c5c683a02cd786e547c0e
JesseDierckx <57669723+JesseDierckx@users.noreply.github.com>
2023-01-09 19:15:50 +0100

    Update main.py

--- eindproject/main.py
+++ eindproject/main.py
@@ -2,10 +2,11 @@ from fastapi import Depends, FastAPI, HTTPException,status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 from datetime import datetime, timedelta
+from jose import JWTError, jwt
+from passlib.context import CryptContext
 
 import os
 
-import auth
 import crud
 import models
 import schemas
@@ -30,18 +31,76 @@ def get_db():
     finally:
         db.close()
 
+SECRET_KEY = "6e34034cae5a1c8acd1a27894bb2027f36e993f5a8aa57e4293d5429f7a9538f"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 480
+
+pwd_context = CryptContext(schemes=["argon2", "bcrypt"], deprecated="auto")
 oauth2_schema = OAuth2PasswordBearer(tokenUrl="token")
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expires_delta = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+def authenticate_shopper(db: Session, username:str, password:str):
+    shopper = crud.get_shopper_by_username(db, username)
+    if not shopper:
+        return False
+    if not verify_password(password, shopper.hashed_password):
+        return False
+    return shopper
+
+def get_current_shopper(db:Session, token: str = Depends(oauth2_schema)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+    except JWTError:
+        raise credentials_exception
+    shopper = crud.get_shopper_by_username(db, username)
+    if shopper is None:
+        raise credentials_exception
+    return shopper
+
+def get_current_active_shopper(db: Session, token: str = Depends(oauth2_schema)):
+    current_shopper = get_current_shopper(db, token)
+    if not current_shopper.is_active:
+        raise HTTPException(status_code=400, detail="Inactive shopper")
+    return current_shopper
+
+
 
 @app.post("/token")
 def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
-    shopper = auth.authenticate_shopper(db, form_data.username, form_data.password)
+    shopper = authenticate_shopper(db, form_data.username, form_data.password)
     if not shopper:
         raise HTTPException(
             status_code=401,
             detail="Incorrect username or password",
             headers={"WWW-Authenticate": "Bearer"}
         )
-    access_token = auth.create_access_token(data={"sub": shopper.username}
+    access_token = create_access_token(data={"sub": shopper.username}
     )
     return {"access_token": access_token, "token_type": "Bearer"}
 
